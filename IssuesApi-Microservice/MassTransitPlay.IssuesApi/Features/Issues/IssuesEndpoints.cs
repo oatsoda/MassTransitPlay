b44b037ad35babb50b5c0f76957f6df7883e84e9
oatsoda <andrew+github@milne.tel>
2023-09-12 16:01:04 +0100

    Add swagger grouping.

--- IssuesApi-Microservice/MassTransitPlay.IssuesApi/Features/Issues/IssuesEndpoints.cs
+++ IssuesApi-Microservice/MassTransitPlay.IssuesApi/Features/Issues/IssuesEndpoints.cs
@@ -6,14 +6,17 @@ public class IssuesEndpoints : IEndpointCollection
     {            
         app.MapGet("/issues", GetList.Execute)
             .WithName("ListIssues")
+            .WithTags("Issues")
             .WithOpenApi(); 
 
         app.MapGet("/issues/{id}", Get.Execute)
             .WithName("GetIssue")
+            .WithTags("Issues")
             .WithOpenApi();
 
         app.MapPost("/issues", Post.Execute)
             .WithName("CreateIssue")
+            .WithTags("Issues")
             .WithOpenApi();
     }
 }
