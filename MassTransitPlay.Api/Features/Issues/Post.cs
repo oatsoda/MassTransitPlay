bc63c7c6b5217ae7654aa158fa1f16e587c6cfba
oatsoda <andrew+github@milne.tel>
2023-09-12 13:52:42 +0100

    Use correct Bus to publish.

--- MassTransitPlay.Api/Features/Issues/Post.cs
+++ MassTransitPlay.Api/Features/Issues/Post.cs
@@ -7,7 +7,7 @@ namespace MassTransitPlay.Api.Features.Issues;
 
 public static class Post
 {
-    public static async Task<IResult> Execute(CreateIssueCommand command, IssueTrackerDbContext dbContext, IBus publish, LinkGenerator linker)
+    public static async Task<IResult> Execute(CreateIssueCommand command, IssueTrackerDbContext dbContext, IPublishEndpoint publish, LinkGenerator linker)
     {
         var issue = new Issue
         {
