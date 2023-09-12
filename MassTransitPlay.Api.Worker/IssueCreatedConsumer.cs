177989d1e86f3e03530b1f105f5c32205acc46ec
oatsoda <andrew+github@milne.tel>
2023-09-12 14:49:50 +0100

    Comment

--- MassTransitPlay.Api.Worker/IssueCreatedConsumer.cs
+++ MassTransitPlay.Api.Worker/IssueCreatedConsumer.cs
@@ -8,6 +8,7 @@ public class IssueCreatedConsumer : IConsumer<IssueCreated>
     public Task Consume(ConsumeContext<IssueCreated> context)
     {
         Console.WriteLine($"CONSUMED! Issue: {context.Message.Id}");
+        // e.g. Send Email or Send SignalR Broadcast Notification or Send Push Notification
         return Task.CompletedTask;
     }
 }
